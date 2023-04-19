6588f7c47e3d3082792e41811bb8bdb10c878888
pecanela <pedro_canelada@yahoo.com.br>
2023-04-19 12:25:28 -0300

    v0

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ let dragging = false;
 let posInicialX, posInicialY;
 let rotacaoX = 0;
 let rotacaoY = 0;
+let velocidadeX = 0;
+let velocidadeY = 0;
+const amortecimento = 0.1;
 
 cubo.addEventListener('mousedown', (event) => {
     dragging = true;
     posInicialX = event.clientX;
     posInicialY = event.clientY;
+    velocidadeX = 0;
+    velocidadeY = 0;
 });
 
 document.addEventListener('mousemove', (event) => {
@@ -17,6 +22,8 @@ document.addEventListener('mousemove', (event) => {
         rotacaoX += difY * 0.01;
         rotacaoY += difX * 0.01;
         cubo.style.transform = `rotateX(${rotacaoX}rad) rotateY(${rotacaoY}rad)`;
+        velocidadeX = difX;
+        velocidadeY = difY;
         posInicialX = event.clientX;
         posInicialY = event.clientY;
     }
@@ -25,6 +32,23 @@ document.addEventListener('mousemove', (event) => {
 document.addEventListener('mouseup', () => {
     dragging = false;
 });
+
+document.addEventListener('mouseout', () => {
+    dragging = false;
+});
+
+function atualizarCubo() {
+    if (!dragging) {
+        velocidadeX *= (1 - amortecimento);
+        velocidadeY *= (1 - amortecimento);
+        rotacaoX += velocidadeY * 0.01;
+        rotacaoY += velocidadeX * 0.01;
+        cubo.style.transform = `rotateX(${rotacaoX}rad) rotateY(${rotacaoY}rad)`;
+    }
+    requestAnimationFrame(atualizarCubo);
+}
+
+requestAnimationFrame(atualizarCubo);
 
 cubo.addEventListener('click', () => {
     cubo.style.transition = 'transform 1s';
