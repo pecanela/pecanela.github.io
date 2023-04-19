740e8fc58ba6f736f974369415b4579825563c76
pecanela <pedro_canelada@yahoo.com.br>
2023-04-19 05:34:49 -0300

    v0

--- script.js
+++ script.js
@@ -3,34 +3,65 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('container').appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);
 
-// Textura do dado
-const loader = new THREE.TextureLoader();
-const texture = loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif');
+// Carregar modelo do dado
+const loader = new THREE.GLTFLoader();
+loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/gltf/Dice.gltf', function (gltf) {
+    scene.add(gltf.scene);
 
-// Geometria e material do dado
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: texture });
-const dice = new THREE.Mesh(geometry, material);
-scene.add(dice);
+    // Configuração da posição e rotação inicial do dado
+    gltf.scene.position.set(0, 0, -5);
+    gltf.scene.rotation.set(0.4, 0.2, 0.1);
+});
 
-// Configuração da posição e rotação inicial do dado
-dice.position.set(0, 0, -5);
-dice.rotation.set(0.4, 0.2, 0.1);
+// Configuração da posição da câmera
+camera.position.z = 5;
 
 // Função para animar o dado
 function animate() {
     requestAnimationFrame(animate);
-    dice.rotation.x += 0.01;
-    dice.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
 // Evento de clique para interação com o dado
-dice.addEventListener('click', () => {
-    dice.rotation.x += 0.5;
-    dice.rotation.y += 0.5;
+document.addEventListener('click', function () {
+    scene.rotation.x += 0.5;
+    scene.rotation.y += 0.5;
+});
+
+// Iniciar animação
+animate();
+// Configuração da cena
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Carregar modelo do dado
+const loader = new THREE.GLTFLoader();
+loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/gltf/Dice.gltf', function (gltf) {
+    scene.add(gltf.scene);
+
+    // Configuração da posição e rotação inicial do dado
+    gltf.scene.position.set(0, 0, -5);
+    gltf.scene.rotation.set(0.4, 0.2, 0.1);
+});
+
+// Configuração da posição da câmera
+camera.position.z = 5;
+
+// Função para animar o dado
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+// Evento de clique para interação com o dado
+document.addEventListener('click', function () {
+    scene.rotation.x += 0.5;
+    scene.rotation.y += 0.5;
 });
 
 // Iniciar animação
