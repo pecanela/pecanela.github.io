a7baebb7423ddc345527b376cc1853cf9e5429b5
pecanela <pedro_canelada@yahoo.com.br>
2023-04-19 05:57:47 -0300

    v0

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('cube').addEventListener('click', function() {
-    this.style.animation = '';
-    void this.offsetWidth;
-    this.style.animation = 'roll 2s infinite';
-  });
-  
