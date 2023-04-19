91adb4e54b444fb3aec1a2c1a92222106b83d928
pecanela <pedro_canelada@yahoo.com.br>
2023-04-19 05:09:15 -0300

    v0

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const canvas = document.getElementById('diceCanvas');
 const ctx = canvas.getContext('2d');
 
+// Variáveis para o movimento do dado
+let rollDirection = '';
+let rollSpeed = 0;
+let rollDistance = 0;
+let rollTotalDistance = 0;
+let rollInterval;
+
 // Função para desenhar o dado 3D
 function drawDice() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -22,10 +29,86 @@ function drawDice() {
     ctx.fill();
 }
 
-// Função para rolar o dado quando o canvas é clicado
-canvas.addEventListener('click', function () {
-    drawDice();
-});
+// Função para rolar o dado
+function rollDice() {
+    if (!rollDirection) {
+        // Gerar um número aleatório de 1 a 6
+        const rollResult = Math.ceil(Math.random() * 6);
+        console.log('Rolled', rollResult);
 
-// Chamar a função de desenhar o dado 3D inicialmente
+        // Definir a direção do rolamento
+        switch (rollResult) {
+            case 1:
+                rollDirection = 'down';
+                break;
+            case 2:
+                rollDirection = 'up';
+                break;
+            case 3:
+                rollDirection = 'right';
+                break;
+            case 4:
+                rollDirection = 'left';
+                break;
+            case 5:
+                rollDirection = 'forward';
+                break;
+            case 6:
+                rollDirection = 'backward';
+                break;
+        }
+
+        // Definir a velocidade e distância do rolamento
+        rollSpeed = 10;
+        rollDistance = 100 + Math.ceil(Math.random() * 100);
+        rollTotalDistance = 0;
+
+        // Iniciar o intervalo de rolamento
+        rollInterval = setInterval(() => {
+            if (rollTotalDistance >= rollDistance) {
+                // Parar o intervalo de rolamento quando a distância total for atingida
+                clearInterval(rollInterval);
+                rollDirection = '';
+                return;
+            }
+
+            // Desenhar o dado em sua posição atual
+            drawDice();
+
+            // Movimentar o dado na direção correta
+            switch (rollDirection) {
+                case 'down':
+                    ctx.translate(0, rollSpeed);
+                    rollTotalDistance += rollSpeed;
+                    break;
+                case 'up':
+                    ctx.translate(0, -rollSpeed);
+                    rollTotalDistance += rollSpeed;
+                    break;
+                case 'right':
+                    ctx.translate(rollSpeed, 0);
+                    rollTotalDistance += rollSpeed;
+                    break;
+                case 'left':
+                    ctx.translate(-rollSpeed, 0);
+                    rollTotalDistance += rollSpeed;
+                    break;
+                case 'forward':
+                    ctx.translate(0, 0, rollSpeed);
+                    rollTotalDistance += rollSpeed;
+                    break;
+                case 'backward':
+                    ctx.translate(0, 0, -rollSpeed);
+                    rollTotalDistance += rollSpeed;
+                    break;
+            }
+        }, 20);
+    }
+}
+
+// Adicionar evento de clique no canvas para rolar o dado
+canvas.addEventListener('click', rollDice);
+
+// Desenhar o dado inicialmente
 drawDice();
+
