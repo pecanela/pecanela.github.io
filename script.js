18f4a081d1476b1d1ad2536c9fc9cd6c891dbea9
pecanela <pedro_canelada@yahoo.com.br>
2023-04-19 05:41:22 -0300

    v0

--- script.js
+++ script.js
@@ -1,68 +1,38 @@
-// Configuração da cena
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+// Configuração inicial da cena
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Carregar modelo do dado
-const loader = new THREE.GLTFLoader();
-loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/gltf/Dice.gltf', function (gltf) {
-    scene.add(gltf.scene);
+// Criação do cubo
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+cube.position.set(0, 0, -5); // Define a posição inicial do cubo
 
-    // Configuração da posição e rotação inicial do dado
-    gltf.scene.position.set(0, 0, -5);
-    gltf.scene.rotation.set(0.4, 0.2, 0.1);
-});
-
-// Configuração da posição da câmera
-camera.position.z = 5;
-
-// Função para animar o dado
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+// Função para atualizar a rotação do cubo
+function updateRotation() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
 }
 
-// Evento de clique para interação com o dado
-document.addEventListener('click', function () {
-    scene.rotation.x += 0.5;
-    scene.rotation.y += 0.5;
-});
-
-// Iniciar animação
-animate();
-// Configuração da cena
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Carregar modelo do dado
-const loader = new THREE.GLTFLoader();
-loader.load('https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/gltf/Dice.gltf', function (gltf) {
-    scene.add(gltf.scene);
-
-    // Configuração da posição e rotação inicial do dado
-    gltf.scene.position.set(0, 0, -5);
-    gltf.scene.rotation.set(0.4, 0.2, 0.1);
-});
-
-// Configuração da posição da câmera
-camera.position.z = 5;
-
-// Função para animar o dado
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+// Função para lidar com o clique no cubo
+function onCubeClick() {
+    cube.rotation.x += Math.random() * Math.PI * 2; // Rotaciona aleatoriamente em torno do eixo x
+    cube.rotation.y += Math.random() * Math.PI * 2; // Rotaciona aleatoriamente em torno do eixo y
+    cube.rotation.z += Math.random() * Math.PI * 2; // Rotaciona aleatoriamente em torno do eixo z
 }
 
-// Evento de clique para interação com o dado
-document.addEventListener('click', function () {
-    scene.rotation.x += 0.5;
-    scene.rotation.y += 0.5;
-});
+// Adiciona o evento de clique no cubo
+cube.addEventListener('click', onCubeClick);
 
-// Iniciar animação
+// Função de renderização
+function animate() {
+    requestAnimationFrame(animate);
+    updateRotation();
+    renderer.render(scene, camera);
+}
 animate();
